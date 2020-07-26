d4f0d833d826033a401016a58c57c071ac8344e2
kraemer-raimund <kraemer_raimund@web.de>
2020-07-26 15:38:21 +0200

    Implement simple health system with UI example

---
+++ Health/Assets/Rakrae.Health/Runtime/Behaviours/HealthSystem.cs
@@ -0,0 +1,39 @@
+﻿/*
+MIT License
+Copyright (c) 2020 Raimund Krämer
+For the full license text please refer to the LICENSE file.
+ */
+
+using Rakrae.Unity.Health.Events;
+using UnityEngine;
+
+namespace Rakrae.Unity.Health.Behaviours
+{
+    public class HealthSystem : MonoBehaviour
+    {
+        [SerializeField] private float _maxHealth = 100.0f;
+        [SerializeField] private float _initialHealth = 100.0f;
+
+        [SerializeField] private HealthInitializedEvent _healthInitialized = null;
+        [SerializeField] private HealthChangedEvent _healthChanged = null;
+
+        private Health _health;
+
+        private void Start()
+        {
+            _health = new Health(_maxHealth, _initialHealth);
+
+            _healthInitialized.Invoke(new HealthInitializedEventArgs(_maxHealth, _initialHealth));
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _health.Reduce(10);
+
+                _healthChanged.Invoke(new HealthChangedEventArgs(_health.CurrentHealth));
+            }
+        }
+    }
+}
---
+++ Health/Assets/Rakrae.Health/Runtime/Events/HealthInitializedEvent.cs
@@ -0,0 +1,16 @@
+﻿/*
+MIT License
+Copyright (c) 2020 Raimund Krämer
+For the full license text please refer to the LICENSE file.
+ */
+
+using System;
+using UnityEngine.Events;
+
+namespace Rakrae.Unity.Health.Events
+{
+    [Serializable]
+    public class HealthInitializedEvent : UnityEvent<HealthInitializedEventArgs>
+    {
+    }
+}
