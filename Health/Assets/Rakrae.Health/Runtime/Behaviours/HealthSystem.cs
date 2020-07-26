3b398478af43347d920f041e5d956a631aa2c91e
kraemer-raimund <kraemer_raimund@web.de>
2020-07-26 18:15:43 +0200

    Refactor starting and stopping of recurring health effects (damage or healing over time) into its own class and delegate to it from the HealthSystem

--- Health/Assets/Rakrae.Health/Runtime/Behaviours/HealthSystem.cs
+++ Health/Assets/Rakrae.Health/Runtime/Behaviours/HealthSystem.cs
@@ -4,8 +4,6 @@ Copyright (c) 2020 Raimund Krämer
 For the full license text please refer to the LICENSE file.
  */
 
-using System.Collections;
-using System.Collections.Generic;
 using Rakrae.Unity.Health.Events;
 using UnityEngine;
 using UnityEngine.Events;
@@ -21,14 +19,15 @@ namespace Rakrae.Unity.Health.Behaviours
         [SerializeField] private HealthChangedEvent _healthChanged = null;
         [SerializeField] private UnityEvent _damageTaken = null;
 
-        private readonly ICollection<Coroutine> _periodicHealingEffects = new HashSet<Coroutine>();
-        private readonly ICollection<Coroutine> _periodicDamageEffects = new HashSet<Coroutine>();
         private Health _health;
-        private Coroutine _selfHealing;
+        private PeriodicHealthEffects _periodicHealthEffects;
 
         private void Start()
         {
             _health = new Health(_maxHealth, _initialHealth);
+            _periodicHealthEffects = new PeriodicHealthEffects(this, _health);
+
+            _periodicHealthEffects.HealthChanged += (s, e) => _healthChanged.Invoke(e);
 
             _healthInitialized.Invoke(new HealthInitializedEventArgs(_maxHealth, _initialHealth));
         }
@@ -55,104 +54,25 @@ namespace Rakrae.Unity.Health.Behaviours
 
             if (Input.GetKeyDown(KeyCode.S))
             {
-                if (_selfHealing != null)
-                {
-                    StopCoroutine(_selfHealing);
-                    _selfHealing = null;
-                }
-                else
-                {
-                    _selfHealing = StartCoroutine(PeriodicHealingEffectCoroutine(new PeriodicHealingEffect(0.5f, 2)));
-                }
+                _periodicHealthEffects.ToggleSelfHealing();
             }
 
             if (Input.GetKeyDown(KeyCode.C))
             {
-                foreach (Coroutine coroutine in _periodicHealingEffects)
-                {
-                    StopCoroutine(coroutine);
-                }
-
-                _periodicHealingEffects.Clear();
-
-                foreach (Coroutine coroutine in _periodicDamageEffects)
-                {
-                    StopCoroutine(coroutine);
-                }
-
-                _periodicDamageEffects.Clear();
-
-                if (_selfHealing != null)
-                {
-                    StopCoroutine(_selfHealing);
-                    _selfHealing = null;
-                }
+                _periodicHealthEffects.ClearAll();
             }
         }
 
         private void AddHealingEffect(PeriodicHealingEffect healingEffect)
         {
-            Coroutine coroutine = StartCoroutine(PeriodicHealingEffectCoroutine(healingEffect));
-            _periodicHealingEffects.Add(coroutine);
-
-            if (healingEffect.DurationSeconds > 0)
-            {
-                StartCoroutine(StopAndRemoveCoroutineAfterDuration(coroutine, _periodicHealingEffects, healingEffect.DurationSeconds));
-            }
+            _periodicHealthEffects.AddHealingEffect(healingEffect);
         }
 
         private void AddDamageEffect(PeriodicDamageEffect damageEffect)
         {
-            Coroutine coroutine = StartCoroutine(PeriodicDamageEffectCoroutine(damageEffect));
-            _periodicDamageEffects.Add(coroutine);
-
-            if (damageEffect.DurationSeconds > 0)
-            {
-                StartCoroutine(StopAndRemoveCoroutineAfterDuration(coroutine, _periodicDamageEffects, damageEffect.DurationSeconds));
-            }
+            _periodicHealthEffects.AddDamageEffect(damageEffect);
         }
 
-        private IEnumerator PeriodicHealingEffectCoroutine(PeriodicHealingEffect healingEffect)
-        {
-            if (healingEffect.FirstTickImmediately)
-            {
-                _health.Heal(healingEffect.HealingPerTick);
-                _healthChanged.Invoke(new HealthChangedEventArgs(_health.CurrentHealth));
-            }
 
-            while (true)
-            {
-                yield return new WaitForSeconds(1 / healingEffect.TicksPerSecond);
-
-                _health.Heal(healingEffect.HealingPerTick);
-                _healthChanged.Invoke(new HealthChangedEventArgs(_health.CurrentHealth));
-            }
-        }
-
-        private IEnumerator PeriodicDamageEffectCoroutine(PeriodicDamageEffect damageEffect)
-        {
-            if (damageEffect.FirstTickImmediately)
-            {
-                _health.Reduce(damageEffect.DamagePerTick);
-                _healthChanged.Invoke(new HealthChangedEventArgs(_health.CurrentHealth));
-            }
-
-            while (true)
-            {
-                yield return new WaitForSeconds(1 / damageEffect.TicksPerSecond);
-
-                _health.Reduce(damageEffect.DamagePerTick);
-                _healthChanged.Invoke(new HealthChangedEventArgs(_health.CurrentHealth));
-            }
-        }
-
-        private IEnumerator StopAndRemoveCoroutineAfterDuration(Coroutine coroutine, ICollection<Coroutine> coroutines, float duration)
-        {
-            yield return new WaitForSeconds(duration);
-
-            _ = coroutines.Remove(coroutine);
-
-            StopCoroutine(coroutine);
-        }
     }
 }
---
+++ Health/Assets/Rakrae.Health/Runtime/PeriodicHealthEffects.cs
@@ -0,0 +1,132 @@
+﻿/*
+MIT License
+Copyright (c) 2020 Raimund Krämer
+For the full license text please refer to the LICENSE file.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rakrae.Unity.Health.Events;
+using UnityEngine;
+
+namespace Rakrae.Unity.Health
+{
+    public class PeriodicHealthEffects
+    {
+        private readonly MonoBehaviour _coroutineOwner;
+        private readonly Health _health;
+        private readonly ICollection<Coroutine> _periodicHealingEffects = new HashSet<Coroutine>();
+        private readonly ICollection<Coroutine> _periodicDamageEffects = new HashSet<Coroutine>();
+        private Coroutine _selfHealing;
+
+        public PeriodicHealthEffects(MonoBehaviour coroutineOwner, Health health)
+        {
+            _coroutineOwner = coroutineOwner;
+            _health = health;
+        }
+
+        public event EventHandler<HealthChangedEventArgs> HealthChanged;
+
+        public void AddHealingEffect(PeriodicHealingEffect healingEffect)
+        {
+            Coroutine coroutine = _coroutineOwner.StartCoroutine(PeriodicHealingEffectCoroutine(healingEffect));
+            _periodicHealingEffects.Add(coroutine);
+
+            if (healingEffect.DurationSeconds > 0)
+            {
+                _coroutineOwner.StartCoroutine(StopAndRemoveCoroutineAfterDuration(coroutine, _periodicHealingEffects, healingEffect.DurationSeconds));
+            }
+        }
+
+        public void AddDamageEffect(PeriodicDamageEffect damageEffect)
+        {
+            Coroutine coroutine = _coroutineOwner.StartCoroutine(PeriodicDamageEffectCoroutine(damageEffect));
+            _periodicDamageEffects.Add(coroutine);
+
+            if (damageEffect.DurationSeconds > 0)
+            {
+                _coroutineOwner.StartCoroutine(StopAndRemoveCoroutineAfterDuration(coroutine, _periodicDamageEffects, damageEffect.DurationSeconds));
+            }
+        }
+
+        public void ToggleSelfHealing()
+        {
+            if (_selfHealing != null)
+            {
+                _coroutineOwner.StopCoroutine(_selfHealing);
+                _selfHealing = null;
+            }
+            else
+            {
+                _selfHealing = _coroutineOwner.StartCoroutine(PeriodicHealingEffectCoroutine(new PeriodicHealingEffect(0.5f, 2)));
+            }
+        }
+
+        public void ClearAll()
+        {
+            foreach (Coroutine coroutine in _periodicHealingEffects)
+            {
+                _coroutineOwner.StopCoroutine(coroutine);
+            }
+
+            _periodicHealingEffects.Clear();
+
+            foreach (Coroutine coroutine in _periodicDamageEffects)
+            {
+                _coroutineOwner.StopCoroutine(coroutine);
+            }
+
+            _periodicDamageEffects.Clear();
+
+            if (_selfHealing != null)
+            {
+                _coroutineOwner.StopCoroutine(_selfHealing);
+                _selfHealing = null;
+            }
+        }
+
+        private IEnumerator PeriodicHealingEffectCoroutine(PeriodicHealingEffect healingEffect)
+        {
+            if (healingEffect.FirstTickImmediately)
+            {
+                _health.Heal(healingEffect.HealingPerTick);
+                HealthChanged?.Invoke(this, new HealthChangedEventArgs(_health.CurrentHealth));
+            }
+
+            while (true)
+            {
+                yield return new WaitForSeconds(1 / healingEffect.TicksPerSecond);
+
+                _health.Heal(healingEffect.HealingPerTick);
+                HealthChanged?.Invoke(this, new HealthChangedEventArgs(_health.CurrentHealth));
+            }
+        }
+
+        private IEnumerator PeriodicDamageEffectCoroutine(PeriodicDamageEffect damageEffect)
+        {
+            if (damageEffect.FirstTickImmediately)
+            {
+                _health.Reduce(damageEffect.DamagePerTick);
+                HealthChanged?.Invoke(this, new HealthChangedEventArgs(_health.CurrentHealth));
+            }
+
+            while (true)
+            {
+                yield return new WaitForSeconds(1 / damageEffect.TicksPerSecond);
+
+                _health.Reduce(damageEffect.DamagePerTick);
+                HealthChanged?.Invoke(this, new HealthChangedEventArgs(_health.CurrentHealth));
+            }
+        }
+
+        private IEnumerator StopAndRemoveCoroutineAfterDuration(Coroutine coroutine, ICollection<Coroutine> coroutines, float duration)
+        {
+            yield return new WaitForSeconds(duration);
+
+            _ = coroutines.Remove(coroutine);
+
+            _coroutineOwner.StopCoroutine(coroutine);
+        }
+    }
+}
